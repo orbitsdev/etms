08adcab78dd182d14ea8efe4e7bc544e7289d95c
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-02-02 22:18:40 +0800

    wip

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,9 +13,15 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+        <style>
+        [x-cloak] {
+            display: none !important;
+        }
 
+    </style>
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @filamentStyles
+        @vite('resources/css/app.css')
 
         <!-- Styles -->
         @livewireStyles
@@ -26,5 +32,8 @@
         </div>
 
         @livewireScripts
+        @livewire('notifications')
+        @filamentScripts
+        @vite('resources/js/app.js')
     </body>
 </html>
