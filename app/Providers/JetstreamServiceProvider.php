1979987b08c8aa70988d411cbcfa00504900739f
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-03-05 12:00:27 +0800

    wip

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Course;
+use Laravel\Fortify\Fortify;
+use Laravel\Jetstream\Jetstream;
 use App\Actions\Jetstream\DeleteUser;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Jetstream\Jetstream;
 
 class JetstreamServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,11 @@ class JetstreamServiceProvider extends ServiceProvider
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
+        Fortify::registerView(function () {
+            return view('auth.register', [
+                'courses' => Course::all(), // Pass all courses to the registration view
+            ]);
+        });
     }
 
     /**
